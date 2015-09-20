89de816115a65bcc7728f80d379f4ac9f4cbfeaf
Chang Liu <shampliu@gmail.com>
2015-09-20 01:08:34 -0700

    images uploaded

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@ var normalPinURL = "http://dailybruin.com/images/2015/05/pin.png";
 var highlightedPinURL = "http://dailybruin.com/images/2015/05/highlighted-pin.png";
 var currentIndex = 9; 
 var highlightedPin = null;
+var starURL = "";
 
 function initialize() {
   gc = new google.maps.Geocoder;
@@ -24,7 +25,7 @@ function initialize() {
         scrollwheel: false,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         center: center,
-        zoom: 10
+        zoom: 9
       });
 
       populateMap(); 
@@ -51,14 +52,14 @@ var previous = function() {
 }
 
 var next = function() {
-  if (currentIndex == 9) {
+  if (currentIndex == 11) {
     return;
   }
 
   currentIndex++;
   clickMarker(currentIndex);
 
-  if (currentIndex == 9) {
+  if (currentIndex == 11) {
     return;
   }
 }
@@ -73,10 +74,24 @@ var populateMap = function() {
       var loc; 
 
       var bubble = document.createElement("BUTTON");        
-      var t = document.createTextNode(index + 1);      
-      bubble.className = "content-bubble";
+      var t = document.createTextNode(index + 1);    
+
+      if (index == 9) {
+        bubble.className = "content-bubble double-digit"
+      }
+      else {
+        bubble.className = "content-bubble";
+      }
       bubble.id = index; 
-      bubble.appendChild(t);
+
+      if (index == 7 || index == 10 || index == 11) {
+        bubble.innerHTML = '&#x2605;'
+        bubble.className = "content-bubble star"
+      }
+      else {
+        bubble.appendChild(t);
+      }
+      
       $(bubble).click(function() {
         clickMarker(bubble.id);
       })
@@ -84,9 +99,17 @@ var populateMap = function() {
       $('#content-nav').append(bubble);
 
       gc.geocode( { 'address' : data[index].address }, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
+        // if (status == google.maps.GeocoderStatus.OK) {
           loc = new google.maps.LatLng(results[0].geometry.location.lat(), results[0].geometry.location.lng());
-        }
+        // }
+
+        var newURL;
+        // if (index == 7 || index == 10 || index == 11) {
+        //   newURL = starURL;
+        // }
+        // else {
+          newURL = normalPinURL;
+        // }
 
         markers[markers.length] = new google.maps.Marker({
           position: loc,
@@ -94,7 +117,7 @@ var populateMap = function() {
           draggable: false,
           // title: "0",
           animation: google.maps.Animation.DROP,
-          icon: normalPinURL
+          icon: newURL
         });
 
         var markerIndex = markers.length-1;
@@ -118,10 +141,16 @@ function clickMarker(i) {
   // refresh the box
   panToIndex(i);
   $('#content-place').html(data[i].cafe);
+  var a = data[i].address.split(',');
   $('#content-address').html(data[i].address);
-  ind = '#' + i; 
+  var ind = '#' + i; 
   $('.active').removeClass('active');
   $(ind).addClass('active');
+  var j = parseInt(i) + 1;
+  $('#content-drink').html(data[i].drink);
+
+  var src = 'img/' + j + '.jpg'
+  $('#content-image img').attr('src', src)
 }
 
 function panToIndex(i) {
@@ -140,7 +169,7 @@ function format(data){
   var result = [];
   var elem = {};
   var real_keyname = '';
-  $.each(data.feed.entry.reverse(), function(i, entry) {
+  $.each(data.feed.entry, function(i, entry) {
     elem = {};
     $.each(entry, function(key, value){
 
